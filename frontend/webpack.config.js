fcbd4bb93c334e671da73b12f287a32061b5ad34
s4f1que3 <safiqueamuel95c@gmail.com>
2025-04-05 21:40:09 -0400

    Revert "fix"
    This reverts commit cca60a510b02e828e3105a403dc9ef826eee854c.
    
    "reverting"

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -10,8 +10,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: '[name].[contenthash:8].js',
-    chunkFilename: '[name].[chunkhash:8].chunk.js',
-    publicPath: './',
+    chunkFilename: '[name].[chunkhash:8].chunk.js', // Ensure unique chunk names
+    publicPath: '/',
     clean: true
   },
   module: {
@@ -76,8 +76,8 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'static/css/[name].[contenthash:8].css',
-      chunkFilename: 'static/css/[name].[chunkhash:8].chunk.css'
+      filename: '[name].[contenthash:8].css',
+      chunkFilename: '[name].[chunkhash:8].chunk.css'
     }),
     new JavaScriptObfuscator({
       rotateStringArray: true,
