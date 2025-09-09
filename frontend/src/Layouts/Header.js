836406e70e29146bd2d223c4e4bc58933788420a
s4f1que3 <safiquesamuel95c@gmail.com>
2025-09-09 12:28:23 -0400

    final changes

--- frontend/src/Layouts/Header.js
+++ frontend/src/Layouts/Header.js
@@ -145,16 +145,7 @@ const Header = () => {
                         </svg>
                         <span className="font-medium">Fan Favorites</span>
                       </Link>
-                      <Link
-                        to="/collections"
-                        className="flex items-center gap-3 rounded-lg px-4 py-3 text-sm text-[#E4D981] bg-[#E4D981]/10"
-                        onClick={handleNavLinkClick}
-                      >
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
-                        </svg>
-                        <span className="font-medium">Collections</span>
-                      </Link>
+                      
                     </div>
                   </div>
 
