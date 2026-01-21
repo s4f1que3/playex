09efdc2612cb1956a284d41caa5bfa870aa327b7
s4f1que3 <safiquesamuel95c@gmail.com>
2026-01-21 15:00:28 -0400

    attempt 3

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -2,5 +2,12 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' && {
+      cssnano: {
+        preset: ['default', {
+          svgo: false, // Disable svgo to avoid parsing issues with SVG content
+        }],
+      },
+    }),
   },
 }
